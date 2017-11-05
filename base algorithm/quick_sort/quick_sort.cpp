e662bc6d73d8a8d905d3104b7993fba3ba39ae76
nil <nilzero@outlook.com>
2017-11-05 21:50:38 +0800

    add base algorithm dir and add quick_sort header and source file

---
+++ base algorithm/quick_sort/quick_sort.cpp
@@ -0,0 +1,4 @@
+#include "quick_sort.h"
+
+
+
---
+++ base algorithm/quick_sort/quick_sort.h
@@ -0,0 +1,21 @@
+#ifndef __QUICK_SORT_H__
+#define __QUICK_SORT_H__
+
+class CQuickSort
+{
+public:
+	CQuickSort();
+	virtual ~CQuickSort();
+
+public:
+	void quick_sort(int *_pArrary, uint _size);
+
+};
+
+
+
+
+
+
+
+#endif
